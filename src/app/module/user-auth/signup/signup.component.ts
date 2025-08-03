7d5ec4ab8839f66642ad35195949af98f7e0bfa8
Sanvi Rahman <SanviRahman>
2025-08-03 12:08:44 +0600

    first commit

---
+++ src/app/module/user-auth/signup/signup.component.ts
@@ -0,0 +1,34 @@
+import { Component, Input, Output } from '@angular/core';
+import { EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent {
+
+  @Input() value: string = 'Sanvi Rahman';
+
+  @Output() valueChange: any= new EventEmitter();
+
+
+  constructor() {
+    console.log('Constructor called');
+  }
+
+
+   ngOnChanges(changes: any) {
+    this.valueChange.emit(this.value);
+    console.log('ngOnChanges called');
+    // console.log(changes);
+  }
+
+  ngOnInit() {
+    console.log('ngOnInit called');
+  }
+
+  ngDoCheck() {
+    console.log('ngDoCheck called');
+  }
+}
